682c3a2d0f99270084f78ddb26e832a42c2a0a56
nithiya <nithiyasrisri3@gmail.com>
2022-12-19 06:08:13 +0530

    adding a file

---
+++ js/math.js
@@ -0,0 +1,17 @@
+//JS math practice
+//x = Math.sqrt(676);
+//x = Math.cbrt(125); 
+//x = Math.abs(90.09);
+//x = Math.random();
+//x = Math.floor(Math.random()*10);
+//x = Math.random()*10;
+//x = Math.pow(8,2); //(m,n)
+//x = Math.floor(-5.9); //returns the lowest integer
+//x = Math.ceil(-9.4); //returns the highest integer
+x = Math.round(8.5); 
+//x = Math.trunc(89.7899);
+//x = Math.sign(10); //positive 1, negative -1, zero 0
+//x = Math.max(76,78,98);
+//x = Math.min(76,78);
+//x = Math.log(7);
+console.log(x);
